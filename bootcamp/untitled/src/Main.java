ae6ca92b5ad8b28386c3f2a4498feea6a0417034
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-15 22:29:20 -0300

    Add files via upload

--- bootcamp/untitled/src/Main.java
+++ bootcamp/untitled/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Clientes cliente = muestraCliente();
+        System.out.println(cliente);
+
+    }
+
+    private static Clientes muestraCliente() {
+        Scanner teclado = new Scanner(System.in);
+        Clientes cliente = new Clientes();
+
+        System.out.println("nombre");
+        cliente.nombre = teclado.nextLine();
+
+        System.out.println("Apellido");
+        cliente.apellido = teclado.nextLine();
+
+        System.out.println("edad");
+        cliente.edad = teclado.nextInt();
+
+        // System.out.println(cliente.nombre + ' ' + cliente.apellido + ' ' + cliente.edad);
+
+
+        return cliente;
+
     }
 }
