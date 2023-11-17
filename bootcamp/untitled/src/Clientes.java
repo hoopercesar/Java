449b6f03a7a2f5503d8aae74593422455306edd8
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-17 07:03:09 -0300

    Add files via upload

--- bootcamp/untitled/src/Clientes.java
+++ bootcamp/untitled/src/Clientes.java
@@ -1,5 +1,35 @@
 public class Clientes {
-    public String nombre;
-    public String apellido;
-    public Integer edad;
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+
+    public Clientes(String parametroNombre, String parametroApellido, Integer parametroEdad) {
+        this.edad = parametroEdad;
+        this.nombre = parametroNombre;
+        this.apellido = parametroApellido;
+    }
+
+    public void mostrarDatos() {
+
+        System.out.println(nombre + ' ' + apellido);
+        System.out.println(edad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+
 }
