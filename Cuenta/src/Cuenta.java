46532d65547a5d66b194a7430f6a31c36ddf2a82
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-17 18:54:22 -0300

    Add files via upload

---
+++ Cuenta/src/Cuenta.java
@@ -0,0 +1,31 @@
+public class Cuenta {
+
+    private String titular;
+    private float cantidad;
+
+
+    public Cuenta(){
+
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(float cantidad){
+        this.cantidad = cantidad;
+    }
+
+    public void mostrar(){
+        System.out.println("");
+    }
+
+}
