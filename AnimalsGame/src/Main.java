8fdfa636b72a69b36d1a15c278ec19d0acac98e4
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-20 15:39:38 -0300

    Add files via upload

--- AnimalsGame/src/Main.java
+++ AnimalsGame/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animals animal = new Animals();
+        System.out.println(animal.muestraAnimal());
     }
 }
