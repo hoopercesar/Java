be2ddef8e48b1ff384be5de114cad4ac25157c74
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-19 22:45:44 -0300

    Add files via upload

--- IntComuesto/src/Main.java
+++ IntComuesto/src/Main.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InteresCompuesto interes = ingresaDatos();
+
+        System.out.println(interes.mostrar());
     }
 
 public static InteresCompuesto ingresaDatos() {
-        
+        Scanner teclado = new Scanner(System.in);
+        InteresCompuesto interes = new InteresCompuesto();
+
+        System.out.println("Ingresa el Monto");
+        interes.setMonto(Float.parseFloat(teclado.nextLine()));
+
+        System.out.println("ingresa tiempo");
+        interes.setTiempo(Integer.valueOf(teclado.nextLine()));
+
+        return interes;
 
 }
 
