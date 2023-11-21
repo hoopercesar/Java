389a0511153c98258651ea4ed69f3f46b867f891
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-20 23:32:39 -0300

    Add files via upload

--- AnimalsGame/src/Main.java
+++ AnimalsGame/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Collection;
+import java.util.Map;
+import java.util.Random;
+
 public class Main {
+    // <>
     public static void main(String[] args) {
         Animals animal = new Animals();
-        System.out.println(animal.muestraAnimal());
+        Random random = new Random();
+        int aleatorio = random.nextInt(6);
+
+        Map<String, String> objetoAnimales = (Map<String, String>) animal.objetoAnimales();
+        Collection<String> animales= objetoAnimales.keySet();
+        Collection<String> frases = objetoAnimales.values();
+
+        String[] arrayAnimals= animales.toArray(new String[0]);
+        String[] arrayFrases = frases.toArray(new String[0]);
+
+        String keyAnimal = arrayAnimals[aleatorio];
+        //String valueFrases = arrayFrases[aleatorio];
+
+        System.out.println(objetoAnimales.get(keyAnimal) + "; " + keyAnimal);
+
+
+
     }
 }
