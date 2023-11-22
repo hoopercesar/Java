367addad118e6be032fd4339739f860d1d2008a8
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-22 20:01:37 -0300

    Add files via upload

--- Basic/src/Main.java
+++ Basic/src/Main.java
@@ -1,6 +1,49 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+
+
         Arreglos arreglos = new Arreglos();
+        Object nombre = ingresaDatos();
         System.out.println(arreglos.sumaNumerosPares());
+        System.out.println(nombre);
+    }
+
+    public static Object ingresaDatos() {
+        // inicializa scanner
+        Scanner teclado = new Scanner(System.in);
+        Estudiantes estudiantes = new Estudiantes();
+
+        List<Object> listaEstudiantes = new ArrayList<>();
+
+        int counter = 0;
+        while (counter < 3) {
+            System.out.println("Ingresa nombre");
+            estudiantes.setNombre(teclado.nextLine());
+
+            System.out.println("edad");
+            estudiantes.setEdad(Integer.parseInt(teclado.nextLine()));
+
+            System.out.println("calificación");
+            estudiantes.setCalificacion(Integer.parseInt(teclado.nextLine()));
+            listaEstudiantes.add(estudiantes);
+
+            counter++;
+        }
+
+        for (Object estudiante: listaEstudiantes) {
+            estudiante.getClass();
+        }
+
+
+        return listaEstudiantes.get(0);
+
+
+
+
+
+
+
     }
 }
