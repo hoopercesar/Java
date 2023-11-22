36ccdaaf7f692e11f90ec96080168bbd3e31531c
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-22 20:01:10 -0300

    Add files via upload

---
+++ Basic/src/Estudiantes.java
@@ -0,0 +1,37 @@
+public class Estudiantes {
+    private String nombre;
+    private int edad;
+
+    private int calificacion;
+
+    public Estudiantes(){
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+        this.edad = edad;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+}
