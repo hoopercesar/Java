0b8a3809b19f03b2c8849ae162368eda2a75c16e
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-22 09:47:27 -0300

    Add files via upload

--- AnimalsGame/src/Main.java
+++ AnimalsGame/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
         Animals animal = new Animals();
         Random random = new Random();
-        int aleatorio = random.nextInt(6);
 
         Map<String, String> objetoAnimales = (Map<String, String>) animal.objetoAnimales();
         Collection<String> animales= objetoAnimales.keySet();
@@ -15,20 +14,36 @@ public class Main {
         String[] arrayAnimals= animales.toArray(new String[0]);
         String[] arrayFrases = frases.toArray(new String[0]);
 
-        String keyAnimal = arrayAnimals[aleatorio];
-        //String valueFrases = arrayFrases[aleatorio];
 
-        System.out.println(objetoAnimales.get(keyAnimal) + "; " + keyAnimal);
-        System.out.println("Qué animal es?");
-        String respuesta = teclado.nextLine();
+        int puntaje = 0;
+        while (puntaje < 6) {
+            int aleatorio = random.nextInt(6-puntaje);
+            String keyAnimal = arrayAnimals[aleatorio];
+            String valueFrases = arrayFrases[aleatorio];
 
-        System.out.println(respuesta + " " + keyAnimal);
-        if (Objects.equals(respuesta, keyAnimal)) {
-            System.out.println("Respuesta correcta");
+            //System.out.println(array);
+
+            System.out.println(valueFrases);
+            System.out.println("Qué animal es?");
+            String respuesta = teclado.nextLine();
+
+            System.out.println(respuesta + " " + keyAnimal);
+            if (!Objects.equals(respuesta, keyAnimal)) {
+                System.out.println("Respuesta INcorrecta");
+            } else {
+                System.out.println("Respuesta Correcta");
+                arrayFrases = removeElement(arrayFrases, keyAnimal);
+                puntaje++;
+                System.out.println(Arrays.toString(arrayFrases));
+            }
+            System.out.println("Tu puntaje : " + puntaje);
         }
+    }
 
-
-
-
+    // Método para eliminar un elemento de un array
+    private static String[] removeElement(String[] array, String element) {
+        List<String> list = new ArrayList<>(Arrays.asList(array));
+        list.remove(element);
+        return list.toArray(new String[0]);
     }
 }
