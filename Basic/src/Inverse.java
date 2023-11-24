0d027de23a329e92db17d27d55214702762f4a4c
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-23 22:52:06 -0300

    Add files via upload

--- Basic/src/Inverse.java
+++ Basic/src/Inverse.java
@@ -1,6 +1,20 @@
+import java.util.List;
+import java.util.Random;
+
 public class Inverse {
+    Random random = new Random();
+
+    Arreglos arreglos = new Arreglos();
+    List<Integer> listaEnteros = arreglos.generaListaEnteros();
+
+    public void algo() {
+        System.out.println(listaEnteros);
+        System.out.println(List.reverse(listaEnteros));
+    }
+
+    //public List<Integer> ordena(){       return Collections.reverse(listaEnteros);   }
 
     public Inverse() {}
 
-    
+
 }
