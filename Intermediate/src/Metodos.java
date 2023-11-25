f9f681be8f8ae8ccf77462e0f619f445dfa66aef
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-25 18:05:57 -0300

    Add files via upload

--- Intermediate/src/Metodos.java
+++ Intermediate/src/Metodos.java
@@ -35,6 +35,29 @@ public class Metodos {
             System.out.println(elem.getPrecio());
         }
 
+        System.out.println("###################################");
+
+        Set<String> nombresUnicos = new HashSet<>();
+        Set<String> nombresDuplicados = new HashSet<>();
+
+        for (Producto elem: listaProductos) {
+            if (!nombresUnicos.add(elem.getNombre())) {
+                // El nombre ya está en el conjunto, por lo tanto, es un duplicado
+                nombresDuplicados.add(elem.getNombre());
+            }
+        }
+
+        //En el contexto de Set, el método add retorna true si el elemento
+        //fue agregado exitosamente al conjunto, lo que indica que el elemento
+        //no estaba presente antes. Retorna false si el elemento ya estaba
+        //presente en el conjunto.
+
+        //Por lo tanto, !nombresUnicos.add(producto.getNombre()) se evalúa como
+        //true si el nombre ya estaba presente en el conjunto (indicando un duplicado),
+        //y false si el nombre no estaba presente y fue agregado correctamente al conjunto.
+
+        System.out.println(nombresDuplicados.size());
+
 
 
         return "resultado___";
