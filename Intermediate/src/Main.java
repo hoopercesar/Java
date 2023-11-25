c2284c7233e31fd249ba123ae7fb88ec749387d4
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-24 22:30:58 -0300

    Add files via upload

--- Intermediate/src/Main.java
+++ Intermediate/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Metodos metodos = new Metodos();
+
+        System.out.println(metodos.ingresaProducto());
+
+
     }
 }
