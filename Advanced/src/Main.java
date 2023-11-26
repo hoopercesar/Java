0de533f742d6dab3ba8c3ccbcab9fc3c13a78799
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-26 08:33:04 -0300

    Add files via upload

--- Advanced/src/Main.java
+++ Advanced/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Exceptions excepcion = new Exceptions();
+
+        System.out.println(excepcion.llenaArreglo().length);
     }
+
+    // <>
 }
