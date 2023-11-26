1dadaa42f600fdbf8762b35d03307cb0ec4003b5
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-25 23:58:57 -0300

    Add files via upload

---
+++ Intermediate/src/Matriz.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Matriz {
+
+    public int[][] llenaMatriz(){
+
+        int filas = 3;
+        int cols = 3;
+        int[][] matriz = new int[filas][cols];
+        Random random = new Random();
+
+        for (int i=0; i<filas; i++) {
+            for (int j=0; j<cols; j++) {
+                matriz = random.nextInt(100);
+
+            }
+        }
+    }
+
+
+}
