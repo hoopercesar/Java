f0ae55b314331655d1c5edabd3cfa8079f9793ca
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-27 10:26:30 -0300

    Add files via upload

--- Advanced/src/Main.java
+++ Advanced/src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Exceptions excepcion = new Exceptions();
+        VerificadorRut verificador = new VerificadorRut();
 
         System.out.println(excepcion.llenaArreglo().length);
+
+        System.out.println(verificador.creaDigito());
+
+
+
     }
 
     // <>
