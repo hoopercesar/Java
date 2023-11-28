b6961a9b47cb4774bb0c3bf0fdec96fc42fa28b3
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-28 10:10:28 -0300

    Add files via upload

--- Advanced/src/Blackjack.java
+++ Advanced/src/Blackjack.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,28 +9,58 @@ public class Blackjack {
 
     // <>
 
-    public static int ingresaCantidadCartas(){
+    public static void ingresaCantidadCartas(){
         Scanner teclado = new Scanner(System.in);
-        Random random = new Random();
-
-        Object[] cartas = {2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"};
 
         System.out.println("Black Jack!");
-        int cantidadCartas = 2;
+        Object[] cartasJugador = (Object[]) entregaCartas(2);
 
-        Object[] cartasJugador = new Object[13];
-
-
-        for (int k=0; k<cantidadCartas; k++) {
-            cartasJugador[k] = cartas[random.nextInt(13)];
-            System.out.println(cartasJugador[k]);
-
+        System.out.println("Estas son tus cartas");
+        for (Object carta: cartasJugador) {
+            System.out.println(carta);
         }
 
+        sumaCartas(cartasJugador);
+        
+        System.out.println("Quieres más cartas? (s/n)");
+        if (Objects.equals(teclado.nextLine(), "s")) {
+
+            System.out.println("Cuántas cartas quieres?");
+            Object[] nuevasCartas = (Object[]) entregaCartas(teclado.nextInt());
+            System.out.println("Ésta es tu nueva carta");
+            for(Object nueva: nuevasCartas){
+                System.out.println(nueva);
+            }
+            sumaCartas(nuevasCartas);
+        } 
+        
+    }
+
+    public static int sumaCartas(Object[] cartasEnMano){
+        int suma = 0;
+        for(Object carta: cartasEnMano) {
+            if (carta=="J" || carta=="Q" || carta=="K") {
+                suma += 10;
+            } else if (carta=="A") {
+                suma += 11;
+            } else {
+                suma += (Integer) carta;
+            }
+        }
+        System.out.println("Esta es la suma: " + suma);
+        return suma;
+    }
 
 
-        return 666;
+    public static Object entregaCartas(int cantidadCartas) {
+        Random random = new Random();
+        Object[] cartas = {2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"};
+        Object[] nuevasCartas = new Object[cantidadCartas];
 
+        for(int k=0; k<cantidadCartas; k++) {
+            nuevasCartas[k] = cartas[random.nextInt(13)];
+        }
+        return nuevasCartas;
     }
 
 
