ac11003fe8f96bd9e646608fbcf7c9735997fb4b
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-12-04 23:16:07 -0300

    Add files via upload

---
+++ servicioElectoral/src/main/java/org/example/Connection.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connection {
+
+    public static void conection() {
+        // Configuración de la conexión
+        String url = "jdbc:mysql://localhost:3306/sistema_votacion";
+        String usuario = "root";
+        String password = "Password123#@!";
+
+        // Intentar establecer la conexión
+        try {
+            // Cargar el controlador JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establecer la conexión
+            java.sql.Connection conexion = DriverManager.getConnection(url, usuario, password);
+            System.out.println("Metadata: " + conexion);
+
+            // Realizar operaciones en la base de datos aquí...
+
+            // Cerrar la conexión cuando hayas terminado
+            conexion.close();
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error al cargar el controlador JDBC: " + e.getMessage());
+        } catch (
+                SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+    }
+}
